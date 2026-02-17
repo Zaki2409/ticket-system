cdc8dede138969f1cc410cc932cca0edf62c2cab
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2026-02-17 16:14:08 +0530

    OPENAI AND ENV

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,7 +11,11 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from dotenv import load_dotenv
 import os  # Added for environment variables
+
+
+load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -132,3 +136,6 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOW_ALL_ORIGINS = True  # For development only
+
+OPENAI_API_KEY = os.getenv('OPENAI_API_KEY')
+
