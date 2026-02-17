fece083dad61379d8c4abb23815d74abd5167604
Zaki2409 <95768544+Zaki2409@users.noreply.github.com>
2026-02-17 15:53:30 +0530

    CORS

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -40,9 +40,11 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'rest_framework',  # Added DRF
     'tickets',         # Added tickets app
+    'corsheaders'
 ]
 
-MIDDLEWARE = [
+MIDDLEWARE = [   
+    'corsheaders.middleware.CorsMiddleware',  
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -129,3 +131,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+CORS_ALLOW_ALL_ORIGINS = True  # For development only
